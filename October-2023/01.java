15cb2e319273bd2f85d9e97d7cf899a4a5a0759b
Dev Shakya <devxoshakya@gmail.com>
2023-10-01 00:31:14 +0530

    init/0110/0032

---
+++ October-2023/01.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int a[][] = new int[n][m];
+            s = in.readLine().trim().split(" ");
+            int ind=0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = Integer.parseInt(s[ind++]);
+                }
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.boundaryTraversal(a, n, m);
+            for (int i : ans) {
+                out.print(i + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    static ArrayList<Integer> boundaryTraversal(int matrix[][], int n, int m)
+    {
+        // code here 
+         ArrayList<Integer> result = new ArrayList<>();
+
+        if (n == 1) {
+            for (int i = 0; i < m; i++) {
+                result.add(matrix[0][i]);
+            }
+        } else if (m == 1) {
+            for (int i = 0; i < n; i++) {
+                result.add(matrix[i][0]);
+            }
+        } else {
+            // Traverse the top row
+            for (int i = 0; i < m; i++) {
+                result.add(matrix[0][i]);
+            }
+            // Traverse the rightmost column
+            for (int i = 1; i < n; i++) {
+                result.add(matrix[i][m - 1]);
+            }
+
+            // Traverse the bottom row (if it exists)
+            if (n > 1) {
+                for (int i = m - 2; i >= 0; i--) {
+                    result.add(matrix[n - 1][i]);
+                }
+            }
+
+            // Traverse the leftmost column (if it exists)
+            if (m > 1) {
+                for (int i = n - 2; i > 0; i--) {
+                    result.add(matrix[i][0]);
+                }
+            }
+        }
+
+        return result;
+    }
+}
